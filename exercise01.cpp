3fe945a6f9811b2c510b577ba08b30c42271f410
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-08-18 11:40:00 -0300

    Make ex. 1 more explicit

--- exercise01.cpp
+++ exercise01.cpp
@@ -3,10 +3,15 @@
 
 int main() {
 	SetConsoleOutputCP(1252);
-	int a = 10, b = 20, c;
+	int a, b, c;
+	printf("Armazenando o valor 10 para A.\n");
+	a = 10;
+	printf("Armazenando o valor 20 para B.\n");
+	b = 20;
+	printf("Invertendo seus valores.\n");
 	c = a;
 	a = b;
 	b = c;
-	printf("O valor de A é %d e o valor de B é %d.", a, b);
+	printf("O valor de A é %d e o valor de B é %d.\n", a, b);
 	return 1;
 }
