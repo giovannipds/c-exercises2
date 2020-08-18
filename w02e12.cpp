4dbe0aa90a08477dac07d6cf5c02773faf702b8c
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-08-18 14:46:37 -0300

    12

---
+++ w02e12.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <windows.h>
+#include <math.h>
+
+int main() {
+	SetConsoleOutputCP(1252);
+	
+	int b, h;
+	float a;
+	
+	printf("Digite o valor da base do triãngulo: ");
+	scanf("%d", &b);
+	
+	printf("Digite a altura do triângulo: ");
+	scanf("%d", &h);
+		
+	a = b * h / 2;
+	printf("A área desse triângulo é de %.2f un².", a);
+	
+	return 1;
+}
