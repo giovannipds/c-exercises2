d78eceaa97c521d0a2319fdb0bd404a1e8c6df63
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-08-18 12:57:52 -0300

    Ex 5

---
+++ w02e05.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <windows.h>
+#include <math.h>
+
+int main() {
+	SetConsoleOutputCP(1252);
+	int v, prev2;
+	printf("Digite um valor qualquer: ");
+	scanf("%d", &v);
+	prev2 = v - 2;
+	printf("O valor anterior de seu antecessor é %d.", prev2);
+	return 1;
+}
