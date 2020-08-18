d6f62d81254d0c519feb7c6f17a988a453253d05
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-08-18 11:49:40 -0300

    Ask for product price

--- w02e02.cpp
+++ w02e02.cpp
@@ -3,9 +3,11 @@
 
 int main() {
 	SetConsoleOutputCP(1252);
-	float p;
+	float paid, price;
 	printf("Qual é o valor a ser pago? ");
-	scanf("%f", &p);
-	printf("O valor pago é %.2f", p);
+	scanf("%f", &paid);
+	printf("Qual é o preço do produto? ");
+	scanf("%f", &price);
+	printf("O valor pago é %.2f, o produto custa %.2f", paid, price);
 	return 1;
 }
