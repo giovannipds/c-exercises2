31d5242323d9ff91896b6cd57bad6d171d06d4e5
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-08-18 11:27:14 -0300

    Finish exercise 1

--- exercise01.cpp
+++ exercise01.cpp
@@ -3,7 +3,10 @@
 
 int main() {
 	SetConsoleOutputCP(1252);
-	int a = 10, b = 20;
+	int a = 10, b = 20, c;
+	c = a;
+	a = b;
+	b = c;
 	printf("O valor de A é %d e o valor de B é %d.", a, b);
 	return 1;
 }
