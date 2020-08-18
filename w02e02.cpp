065c3cb026e7a5537def043e5d70af6acd925018
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-08-18 12:01:03 -0300

    Finish e02

--- w02e02.cpp
+++ w02e02.cpp
@@ -3,11 +3,20 @@
 
 int main() {
 	SetConsoleOutputCP(1252);
-	float paid, price;
-	printf("Qual é o valor a ser pago? ");
+	float paid, price, exchange;
+	printf("Qual é o valor a pago? ");
 	scanf("%f", &paid);
 	printf("Qual é o preço do produto? ");
 	scanf("%f", &price);
-	printf("O valor pago é %.2f, o produto custa %.2f", paid, price);
+	exchange = abs(price - paid);
+	printf("O valor pago foi de R$ %.2f.\n", paid);
+	printf("O produto custa R$ %.2f.\n", price);
+	if (price > paid) {
+		printf("Damos R$ %.2f de desconto pro cliente ou cobramos essa diferença.", exchange);
+	} else if (price < paid) {
+		printf("O troco deve ser de R$ %.2f\n", exchange);
+	} else {
+		printf("Não é necessário troco.");
+	}
 	return 1;
 }
